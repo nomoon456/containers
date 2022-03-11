6cd5b7a188966fb851cb838af3f7aca127ff074a
Elliot BOUJU <elbouju@e2r4p1.clusters.42paris.fr>
2022-03-11 16:52:27 +0100

    zzzzz

--- include/const_reverse_iterator_vector.hpp
+++ include/const_reverse_iterator_vector.hpp
@@ -48,10 +48,13 @@ public:
 	bool operator <=(const_reverse_iterator_vector const& b) const { return (_base <= b._base); };
 
 	// ARITHMETICS
-	const_reverse_iterator_vector operator +(difference_type b) { return (const_reverse_iterator_vector(_base - b)); }; // a + b
-	const_reverse_iterator_vector operator -(difference_type b) { return (const_reverse_iterator_vector(_base + b)); }; // a - b
+	const_reverse_iterator_vector operator +(const difference_type b) const { return (_base - b); }; // a + b
+	const_reverse_iterator_vector operator -(difference_type b) const { return (_base + b); }; // a - b
 
-	difference_type operator +(const_reverse_iterator_vector b) { return (_base - b._base); }; // a + b
+friend const_reverse_iterator_vector	operator+(difference_type n, const const_reverse_iterator_vector &rhs)
+				{ return rhs.operator+(n); };
+
+	// difference_type operator +(const_reverse_iterator_vector b) { return (_base - b._base); }; // a + b
 	difference_type operator -(const_reverse_iterator_vector b) { return (_base + b._base); }; // a - b
 
 	// INCREMENTERS
@@ -61,11 +64,12 @@ public:
 	const_reverse_iterator_vector operator --(int) { _base++; return (const_reverse_iterator_vector(_base - 1)); };	// a--
 
 	//COMPOUND ASSIGNMENTS
-	void operator +=(difference_type b) { _base -= b; };	// a += b
-	void operator -=(difference_type b) { _base += b; };	// a -= b
+	const_reverse_iterator_vector operator +=(difference_type b) { _base -= b; };	// a += b
+	const_reverse_iterator_vector operator -=(difference_type b) { _base += b; };	// a -= b
 
 	//DEREFERENCING & ADDRESS STUFF
-	const_reference operator *() const { return (*_base); };								// *a
+	const_reference operator *() const { return (*this->_base); };
+	reference operator *() { return (this->_base); };							// *a
 	reference operator [](difference_type b) { return (*(_base + b)); };					// a[]
 	const_reference operator [](difference_type b) const { return (*(_base + b)); };		// a[]
 	pointer operator ->() { return (_base); };											// a->b
@@ -74,54 +78,16 @@ public:
 	static const bool input_iter = true;
 
 	private:
-		const_reverse_iterator_vector(pointer ptr) { _base = ptr; };
-		pointer _base;
+		const_reverse_iterator_vector(iterator ptr) { _base = ptr; };
+		iterator _base;
 };
+
+template <class T>
+const_reverse_iterator_vector<T> operator+(std::ptrdiff_t i, const_reverse_iterator_vector<T> &it) {
+	return (it - i);
+	}
+
 
 }
 
 #endif
-
-// namespace ft {
-
-//     template <typename T>
-//     class	const_reverse_iterator_vector : public iterator<T> {
-//     public:
-//         typedef const_reverse_iterator_vector<T> _it;
-
-//         typedef std::ptrdiff_t difference_type;
-// 	    typedef T value_type;
-// 	    typedef	T*  pointer;
-// 		typedef T& reference;
-//         typedef const T& const_reference;
-//         typedef const T* const_pointer;
-// 		typedef ft::random_access_iterator_tag iterator_category;
-
-// 	    const_reverse_iterator_vector(void) : iterator<T>(){};
-// 	    const_reverse_iterator_vector(pointer ptr) {this->_base = ptr; };
-// 	    ~const_reverse_iterator_vector() {};
-// 		// template <class m> const_reverse_iterator(const const_reverse_iterator_vector<m> &u) : _base(this->_base) { };
-// 		const_reverse_iterator_vector(const iterator<T> &src) : iterator<T>(src) {};
-
-// 	    const _it operator+(difference_type p) {return(_it(this->_base - p));}; // _base + p
-// 	    const _it operator-(difference_type p) {return(_it(this->_base + p));}; // _base - p
-
-// 	    difference_type operator+(_it p) const {return(this->_base - p._base);}; // _base + p
-// 	    difference_type operator-(_it p) const {return(this->_base + p._base);}; // _base - p
-
-// 	    const _it operator++() { --this->_base; return(*this);}; // ++_base
-// 		const _it operator++(int) {_it tmp(*this); --this->_base; return(tmp);}; // _base++
-// 		const _it operator--() { ++this->_base; return(*this);}; // --_base
-// 		const _it operator--(int) {_it tmp(*this); ++this->_base; return(tmp);}; // _base--
-
-// 	    _it operator+=(difference_type p) {this->_base -= p; return(*this);}; // _base += p
-// 	    _it operator-=(difference_type p) {this->_base += p; return(*this);}; // _base -= p
-
-// 	    const_pointer operator->() {return(this->_base);}; // _base->p
-// 	    const_pointer operator->() const {return (this->_base);};	
-//         const_reference operator*() const {return (*this->_base);}; // *_base
-// 	    const_reference operator[](difference_type p) const {return (*(this->_base + p));}; // _base[]
-//     }; //end of iterator_vector definition class
-// }//end of namespace ft
-
-// #endif
--- include/reverse_iterator_vector.hpp
+++ include/reverse_iterator_vector.hpp
@@ -1,5 +1,10 @@
-#ifndef VECTOR_REVERSE_ITERATOR_HPP
-# define VECTOR_REVERSE_ITERATOR_HPP
+#ifndef VECTOR_REVERSE_ITERATOR_vector_HPP
+# define VECTOR_REVERSE_ITERATOR_vector_HPP
+
+#include "../utils/traits.hpp"
+#include "iterator.hpp"
+#include "iterator_vector.hpp"
+#include <cstddef>
 
 namespace ft
 {
@@ -26,7 +31,7 @@ public:
 	// typedef iterator	difference_type;
 
 	reverse_iterator_vector(void) {};
-	reverse_iterator_vector(pointer ptr) { _base = ptr; };
+	reverse_iterator_vector(pointer ptr) { this->_base = *ptr; };
 
     // vectorReverseIterator(iterator_vector<value_type> const &src) { _base = src.operator->(); } ;
 	// vectorReverseIterator(vectorReverseIterator const &src) { *this = src; } ;
@@ -49,12 +54,14 @@ public:
 	bool operator <=(reverse_iterator_vector const& b) const { return (_base <= b._base); };
 
 	// ARITHMETICS
-	// reverse_iterator_vector operator +(difference_type b) { return (reverse_iterator_vector(_base - b)); }; // a + b
-	reverse_iterator_vector	operator+ (difference_type n) { return reverse_iterator_vector(this->_base.operator-(n)); };
-	reverse_iterator_vector operator -(difference_type b) { return (reverse_iterator_vector(_base + b)); }; // a - b
 
-	difference_type operator +(reverse_iterator_vector b) { return (_base - b._base); }; // a + b
-	difference_type operator -(reverse_iterator_vector b) { return (_base + b._base); }; // a - b
+	// difference_type		operator-(const reverse_iterator<U> &u) { return u.base().operator-(this->_base); };
+	// reverse_iterator	operator- (difference_type n) const { return reverse_iterator(this->_base.operator+(n)); };
+
+	reverse_iterator_vector	operator+ (difference_type n) const { return reverse_iterator_vector(_base.operator-(n)); };
+	reverse_iterator_vector operator +(const reverse_iterator_vector b) { return (_base - b._base); }; // a + b
+	reverse_iterator_vector operator -(const reverse_iterator_vector b) { return (_base + b._base); }; // a - b
+	reverse_iterator_vector	operator- (difference_type n) const { return reverse_iterator_vector(this->_base.operator+(n)); };
 
 	// INCREMENTERS
 	reverse_iterator_vector operator ++() { _base--; return (*this); };			// ++a
@@ -63,16 +70,18 @@ public:
 	reverse_iterator_vector operator --(int) { _base++; return (reverse_iterator_vector(_base - 1)); };	// a--
 
 	//COMPOUND ASSIGNMENTS
-	void operator +=(difference_type b) { _base -= b; };	// a += b
-	void operator -=(difference_type b) { _base += b; };	// a -= b
+	reverse_iterator_vector operator +=(difference_type b) { _base -= b; };	// a += b
+	reverse_iterator_vector operator -=(difference_type b) { _base += b; };	// a -= b
 
 	//DEREFERENCING & ADDRESS STUFF
-	reference operator *() { return (*_base); };											// *a
-	const_reference operator *() const { return (*_base); };								// *a
-	reference operator [](difference_type b) { return (*(_base + b)); };					// a[]
-	const_reference operator [](difference_type b) const { return (*(_base + b)); };		// a[]
-	pointer operator ->() { return (_base); };											// a->b
-	pointer operator ->() const { return (_base); };											// a->b
+
+	reference operator *() { return (this->_base); };											// *a
+	const_reference operator *() const { return (*this->_base); };								// *a
+	// reference operator [](difference_type b) { return ((this->_base + b)); };				// a[]
+	// const_reference operator [](difference_type b) const { return (*(this->_base + b)); };		// a[]
+	reference	operator[](difference_type n)  { return *this->operator+(n); };
+	iterator operator ->() { return (this->_base); };											// a->b
+	iterator operator ->() const { return (this->_base); };											// a->b	
 
 	static const bool input_iter = true;
 
@@ -87,72 +96,12 @@ reverse_iterator_vector<Iterator>	&reverse_iterator_vector<Iterator>::operator=(
 	this->_base = u.base();
 	return (*this); }
 
+template <class T>
+reverse_iterator_vector<T> operator+(std::ptrdiff_t i, reverse_iterator_vector<T> &it) {
+	return (it - i);
+}
+
 
 }
 
 #endif
-
-// #ifndef REVERSE_ITERATOR_VECTOR_HPP
-// #define REVERSE_ITERATOR_VECTOR_HPP
-
-// #include "../utils/traits.hpp"
-// // #include "rev_ite.hpp"
-// #include "iterator.hpp"
-
-// namespace ft{
-
-//     template <class T>
-//     class reverse_iterator_vector : public iterator<T> {
-//     public:
-//         typedef reverse_iterator_vector<T> _it;
-//         typedef std::ptrdiff_t difference_type; 
-// 	    typedef T value_type;
-// 	    typedef	T*  pointer;
-// 		typedef const T* const_pointer;
-// 	    typedef T& reference;
-//         typedef const T& const_reference;
-// 		typedef ft::random_access_iterator_tag iterator_category;
-
-// 		reverse_iterator_vector(void) : iterator<T>(){};
-//         reverse_iterator_vector(pointer ptr){this->_base = ptr;};
-//         ~reverse_iterator_vector(){};
-//         reverse_iterator_vector(const reverse_iterator_vector &src) : iterator<T>(src) {};
-//         reverse_iterator_vector(int src) {this->_base =src;};
-
-// 		_it operator=(const reverse_iterator_vector &rhs) {
-// 			if (this == &rhs)
-// 				return (*this);
-// 			this->_base = rhs._base;
-// 			return (*this);
-// 		}
-
-// 		_it operator +(difference_type p) {return this->_base - p;};
-// 	    _it operator +(difference_type p) const {return(_it(this->_base - p));}; // _base + p
-// 	    _it operator -(difference_type p) const {return(_it(this->_base + p));}; // _base - p
-
-// 	    difference_type operator +(_it p) {return(this->_base - p._base);}; // _base + p
-// 	    difference_type operator -(_it p) {return(this->_base + p._base);}; // _base - p
-
-// 	    _it operator++() { --this->_base; return(*this);}; // ++_base
-// 		_it operator++(int) {_it tmp(*this); --this->_base; return(tmp);}; // _base++
-// 		_it operator--() { ++this->_base; return(*this);}; // --_base
-// 		_it operator--(int) {_it tmp(*this); ++this->_base; return(tmp);}; // _base--
-
-// 	    _it operator +=(difference_type p) {this->_base -= p; return(*this);}; // _base += p
-// 	    _it operator -=(difference_type p) {this->_base += p; return(*this);}; // _base -= p
-
-// 	    pointer operator ->() {return(this->_base);}; // _base->p
-// 	    pointer operator ->() const {return (this->_base);};	
-// 		reference operator *() {return (*this->_base);}; // *_base
-//         reference operator *() const {return (*this->_base);}; // *_base
-// 	    reference operator [](difference_type p) const {return (*(this->_base + p));}; // _base[]
-// 		reference operator [](difference_type p) { return (*(this->_base + p)); };	
-
-//     }; //end of reverse_iterator_vector definition class
-// 	template <class T>
-// 	reverse_iterator_vector<T> operator+(std::ptrdiff_t i, reverse_iterator_vector<T> &it) {
-// 		return (it - i);
-// 	}
-// }//end of namespace ft
-
-// #endif
